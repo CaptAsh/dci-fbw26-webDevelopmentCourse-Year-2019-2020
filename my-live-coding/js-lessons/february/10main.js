a1a7eb5183d27767f0ae70ea4f78f2cc4422c367
Ash-dci <ashraf-dci@web.de>
2020-02-10 22:06:09 +0100

    new commit

--- my-live-coding/js-lessons/february/10main.js
+++ my-live-coding/js-lessons/february/10main.js
@@ -3,6 +3,8 @@ COPY FROM LECTURER
 HOME OFFICE
 LESSON*/
 
+console.log("::::::::::::::end1::::::::::::");
+
 
 // Compound object
 
@@ -20,36 +22,64 @@ const objects = [
     },
     {
       name: "Nancy",
-      lastName: "Aii"
+      lastName: "Ali"
     }
   ];
   // dot.
-  objects[1].printOut();
+  objects[1].printOut();                          // Hi
+
+
+  console.log("::::::::::::::end2::::::::::::");
   
   // 2nd way to call bracket []
-  objects[1]["printOut"]();
+  objects[1]["printOut"]();                         // Hi
+
+  console.log("::::::::::::::end3::::::::::::");
 
 
   // Clones vs. References
+
+
+
+  
+  
   
   // Primitive  (Simple values are always clones)
   // String, Number, Boolean
   let str = "How are you";
-  let newStr = str; // cloning
+  let newStr = str;    // cloning
   
   str = "something else";
-  console.log(newStr);
-  console.log(str);
+  console.log(newStr);             //How are you
+  console.log(str);                //something else
+
+  console.log("::::::::::::::end3::::::::::::");
+
   let num = 222;
   let newNum = num;
-  console.log(newNum);
+  console.log(newNum);  // 222
+
+  console.log("::::::::::::::end::::::::::::");
+
+
+  ////////////////////////////////////////////
   // Compound
   // Object, Array, Function
+
   const names = ["Ali", "Olga", "Nancy", "Zain"];
-  let newArr = names; //
-  console.log(newArr);
+  let newArr = names;    // cloning
+  console.log(newArr);  //                  [ 'Ali', 'Olga', 'Nancy', 'Zain' ]
+
+  console.log("::::::::::::::end::::::::::::");
+  
   newArr[1] = "New";
-  console.log(names);
+  console.log(names); //                  [ 'Ali', 'New', 'Nancy', 'Zain' ]
+
+  console.log("::::::::::::::end::::::::::::");
+
+///////////////////////////////////
+
+
   const car = {
     name: "Ford",
     year: "2020",
@@ -58,44 +88,82 @@ const objects = [
   
   const newObject = car;
   newObject.name = "BMW";
-  console.log(car.name);
-  
+  console.log(car.name);  //         BMW
+
+
+
+console.log("::::::::::::::end spread op::::::::::::");
+
   
   
   // Objects are references by default: Array and object literals
   
 
 
-  // Shallow copy using ... (spread operator)
+  // Shallow copy using    ... (spread operator)
   let namesArr = ["Ali", "Hadi"];
   let newNamesArr = [...namesArr];
   newNamesArr[0] = "Nancy";
-  console.log(newNamesArr);
-  console.log(namesArr);
-  // Shallow copy using .concat()
-  let newNames2 = [].concat(namesArr);
+  console.log(newNamesArr);    // [ 'Nancy', 'Hadi' ]
+  console.log(namesArr);       //  [ 'Ali', 'Hadi' ]
+
   
+  console.log("::::::::::::::end spread op::::::::::::");
+
+
+  // Shallow copy using .concat()
+  let newNames2 = [].concat(namesArr);  // why empty array.concat? merge with what?
+
   // Shallow copy using .slice()
   let newNames3 = namesArr.slice(0);
-  console.log(newNames3);
+  console.log(newNames3);            //  [ 'Ali', 'Hadi' ] // must ask more!!
+
+  console.log("::::::::::::::end::::::::::::");
+
+
   // Shallow copy using .assign()
   const newObject2 = Object.assign(car);
-  const mewObject3 = { ...car };
-  //  Recursion
-  
-  // in one word it's a way to create a function that calls itself and being able to stope calling itself when it done the job, otherwise it will be infinity loop.
-  // the stack call when its ended js able to unwind the stack and return the result
+  console.log(newObject2);                 // { name: 'BMW', year: '2020', color: 'Red' }
+
+  console.log("::::::::::::::end::::::::::::");
+
+
+// Shallow copy using ... (spread operator)
+  const newObject3 = { ...car };
+  console.log(newObject3);                // { name: 'BMW', year: '2020', color: 'Red' }
+
+
+  console.log("::::::::::::::end shallow copy::::::::::::");
+
+  console.log("::::::::::::::here start RECURSION:::::::::::");
+
+
+  //  Recursion(the repeated application of a recursive procedure or definition)
+  // in one word it's a way to create a function that calls itself and being able to stop
+  // calling itself when it done the job, otherwise it will be infinity loop.
+  // the stack call when its ended js able to unwind the stack and return the result!
+
   
   const factorial = function(num) {
     // console.log(num);
     if (num == 1) {
       return 1;
-    }
+    }                               // no else here ??? heheh
     return num * factorial(num - 1);
+        // 5   *           5   - 1 = 5 * (5-1) =  20   // 
   };
-  console.log(factorial(5));
+  console.log(factorial(5));               // 120 why  i got 120 ?? 
   
+  console.log(":::::::end RECURSION:::::::::::");
+  
+
+  
+////////////////////////////////////////////////////////////
+
+console.log(":::::::Deep copy using RECURSION:::::::::::");
+
   // Deep copy using recursion 🤨
+
   function deepCopy(obj) {
     if (typeof obj == "object") {
       return Object.keys(obj)
